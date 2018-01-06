b0008e695b8f42ea273069572b0d4e686858cee8
dbicanic <dbicanic77@gmail.com>
2018-01-06 08:54:17 -0600

    Todo list is finished and wonrking

--- todolist/src/TodoItems.js
+++ todolist/src/TodoItems.js
@@ -2,26 +2,27 @@ import React, { Component } from "react";
 import FlipMove from 'react-flip-move';
  
 class TodoItems extends Component {
-  constructor(props, context) {
-    super(props, context);
- 
-    this.createTasks = this.createTasks.bind(this);
-  }
- 
-  createTasks(item) {
-    return <li key={item.key}>{item.text}</li>
-  }
- 
-  render() {
-    var todoEntries = this.props.entries;
-    var listItems = todoEntries.map(this.createTasks);
- 
-    return (
-      <ul className="theList">
-          {listItems}
-      </ul>
-    );
-  }
+	constructor(props, context) {
+		super(props, context);
+		this.createTasks = this.createTasks.bind(this);
+	}
+
+	createTasks(item) {
+			return <li key={item.key}>{item.text}</li>
+	}
+
+	render() {
+		var todoEntries = this.props.entries;
+		var listItems = todoEntries.map(this.createTasks);
+
+		return (
+			<ul className="theList">
+				<FlipMove duration={250} easing="ease-out">
+					{listItems}
+				</FlipMove>
+			</ul>
+		);
+	}
 };
- 
+
 export default TodoItems;
